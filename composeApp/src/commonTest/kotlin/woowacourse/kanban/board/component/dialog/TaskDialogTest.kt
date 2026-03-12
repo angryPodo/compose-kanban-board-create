7b8108325b8cfd8ecd5ed816f6f16d0b3e76acf5
hanminjae <hanminjae131@gmail.com>
2026-03-12 14:54:54 +0900

    test: TaskDialog 생성 버튼 활성화 조건 테스트 추가
    - TaskDialogTest 클래스 레벨로 @OptIn 어노테이션 이동
    - 제목 입력 및 유효한 태그(1~5자, 1개) 입력 시 생성 버튼 활성화 검증 테스트 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/dialog/TaskDialogTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/dialog/TaskDialogTest.kt
@@ -9,9 +9,8 @@ import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
 
+@OptIn(ExperimentalTestApi::class)
 class TaskDialogTest {
-
-    @OptIn(ExperimentalTestApi::class)
     @Test
     fun `초기 렌더링 시 제목이 비어있으므로 생성 버튼이 비활성화된다`() = runComposeUiTest {
         // Given
@@ -28,7 +27,7 @@ class TaskDialogTest {
         onNodeWithText("생성").assertIsNotEnabled()
     }
 
-    @OptIn(ExperimentalTestApi::class)
+
     @Test
     fun `제목을 입력하면 생성 버튼이 활성화된다`() = runComposeUiTest {
         // Given
@@ -46,7 +45,26 @@ class TaskDialogTest {
         onNodeWithText("생성").assertIsEnabled()
     }
 
-    @OptIn(ExperimentalTestApi::class)
+
+    @Test
+    fun `제목을 입력하고 태그 개수가 1개이면서 1~5자 이내일 경우 생성 버튼이 활성화된다`() = runComposeUiTest {
+        // Given
+        setContent {
+            TaskDialog(
+                onCreateClick = { _, _, _, _, _ -> },
+                onDismissClick = {},
+            )
+        }
+
+        // When
+        onNodeWithText("태스크 제목을 입력하세요.").performTextInput("안드로이드 과제하기")
+        onNodeWithText("태그를 쉼표로 구분하여 입력하세요 (예: 버그, 긴급)").performTextInput("정상태그")
+
+        // Then
+        onNodeWithText("5자 이내의 태그를 최대 5개까지 등록할 수 있습니다.").assertIsDisplayed()
+        onNodeWithText("생성").assertIsEnabled()
+    }
+
     @Test
     fun `제목을 입력하고 6자 이상의 태그를 입력하면 생성 버튼이 비활성화되고 에러 메시지가 노출된다`() = runComposeUiTest {
         // Given
@@ -66,7 +84,7 @@ class TaskDialogTest {
         onNodeWithText("생성").assertIsNotEnabled()
     }
 
-    @OptIn(ExperimentalTestApi::class)
+
     @Test
     fun `제목을 입력하고 태그를 6개 입력하면 생성 버튼이 비활성화되고 에러 메시지가 노출된다`() = runComposeUiTest {
         // Given
