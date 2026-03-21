3ac87527b5d269eaece66be56130edaafd8541f9
hanminjae <hanminjae131@gmail.com>
2026-03-21 21:36:32 +0900

    feat: KanbanColumn 컴포넌트 및 커스텀 컬러 추가

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/Color.kt
@@ -0,0 +1,19 @@
+package woowacourse.kanban.board
+
+import androidx.compose.ui.graphics.Color
+
+val Color.Companion.LightBlue: Color
+    get() = Color(0xFFEFF6FF)
+
+val Color.Companion.Orange: Color
+    get() = Color(0xFFE17100)
+
+val Color.Companion.LightYellow: Color
+    get() = Color(0xFFFFFBEB)
+
+val Color.Companion.DeepGreen: Color
+    get() = Color(0xFF00A63E)
+
+val Color.Companion.LightGreen: Color
+    get() = Color(0xFFF0FDF4)
+
